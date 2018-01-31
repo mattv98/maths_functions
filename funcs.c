e1e1a3dd84c5afa83472e3eb629bf06e3a157ed0
test name <test@email.com>
2018-01-31 07:31:14 -0800

    Added a function to workout a remainder between 2 numbers

--- funcs.c
+++ funcs.c
@@ -7,7 +7,7 @@ int main()
 	int i, n, sum = 0, num;
 	float avg;	
 	
-	printf("1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n5:Average\n");
+	printf("1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n5:Average\n6:Remainder\n");
 	printf("Enter the number correspoding to the function you want to perform:\n");
 	scanf("%d",&opt);
 	
@@ -62,7 +62,14 @@ int main()
 			printf("The average of those numbers is %f\n", avg);
 				
 		break;
+		
+		//Remainder
+		case 6:
+			printf("Enter 2 variables:\n");
+			scanf("%d%d", &var1, &var2);
 
+			printf("%d mod %d = %d\n", var1, var2, var1 % var2); 				
+	
 		//If wrong option is selected
 		default:
 			printf("Option not on the list\n");
